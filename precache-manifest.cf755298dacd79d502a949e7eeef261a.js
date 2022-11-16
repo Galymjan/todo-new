53564e07d00479205788398c1cfe0aa04bc043ef
Galymjan <galymjant@gmail.com>
2022-11-16 19:00:12 +0600

    Updates

---
+++ precache-manifest.cf755298dacd79d502a949e7eeef261a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60a2f72a6226256128cb",
+    "url": "/todo-new/static/css/main.ad81fb6e.chunk.css"
+  },
+  {
+    "revision": "60a2f72a6226256128cb",
+    "url": "/todo-new/static/js/main.60a2f72a.chunk.js"
+  },
+  {
+    "revision": "a563b2edb5d9163ec13a",
+    "url": "/todo-new/static/css/1.fa36f668.chunk.css"
+  },
+  {
+    "revision": "a563b2edb5d9163ec13a",
+    "url": "/todo-new/static/js/1.a563b2ed.chunk.js"
+  },
+  {
+    "revision": "5060d96db6fbc7ca7d32",
+    "url": "/todo-new/static/js/runtime~main.5060d96d.js"
+  },
+  {
+    "revision": "baf33d8a8472a9db79e90d33ee96137b",
+    "url": "/todo-new/index.html"
+  }
+];
